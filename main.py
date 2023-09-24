83cb312a53f8d88d2a0b7d49b55edcd7c6561243
ROSTIG-NOX3N <137249382+ROSTIG-NOX3N@users.noreply.github.com>
2023-09-25 01:37:44 +0900

    Update main.py

--- main.py
+++ main.py
@@ -291,204 +291,196 @@ for question in question_options:
 # Streamlit 앱의 제목을 설정합니다.
 st.title("부산 여행 추천 및 위치 표시")
 
-# 사용자의 질문을 입력받습니다.
-user_question = st.selectbox("질문을 선택하세요", [
-    "부산에서 가볼만한 관광지에 대해 추천해줘",
-    "부산에서 가볼만한 맛집에 대해 추천해줘",
-    "부산에서 가볼만한 자연/공원에 대해 추천해줘",
-    "부산에서 가볼만한 카페/디저트에 대해 추천해줘"
-])
-
 # 사용자의 질문에 따른 답변과 위치 정보를 설정합니다.
-answer = ""
+
 location = None
 
 if user_question == "부산에서 가볼만한 관광지에 대해 추천해줘":
-    answer = 용궁사1
+    answers = 용궁사1
     location = [35.5368, 129.2051]  # (해동용궁사)
 
 elif user_question == "부산에서 가볼만한 관광지에 대해 추천해줘":
-    answer = 용궁사2
+    answers = 용궁사2
     location = [35.5368, 129.2051]  # (해동용궁사)
 
 elif user_question == "부산에서 가볼만한 관광지에 대해 추천해줘":
-    answer = 용궁사3
+    answers = 용궁사3
     location = [35.5368, 129.2051]  # (해동용궁사)
 
 elif user_question == "부산에서 가볼만한 관광지에 대해 추천해줘":
-    answer = 용궁사4
+    answers = 용궁사4
     location = [35.5368, 129.2051]  # (해동용궁사)
 
 elif user_question == "부산에서 가볼만한 관광지에 대해 추천해줘":
-    answer = 해운대1
+    answers = 해운대1
     location = [35.1631, 129.1635]  # (해운대)
 
 elif user_question == "부산에서 가볼만한 관광지에 대해 추천해줘":
-    answer = 해운대2
+    answers = 해운대2
     location = [35.1631, 129.1635]  # (해운대)
 
 elif user_question == "부산에서 가볼만한 관광지에 대해 추천해줘":
-    answer = 해운대3
+    answers = 해운대3
     location = [35.1631, 129.1635]  # (해운대)
 
 elif user_question == "부산에서 가볼만한 관광지에 대해 추천해줘":
-    answer = 남포동1
+    answers = 남포동1
     location = [35.0673, 129.0916]  # (남포동)
 
 elif user_question == "부산에서 가볼만한 관광지에 대해 추천해줘":
-    answer = 남포동2
+    answers = 남포동2
     location = [35.0673, 129.0916]  # (남포동)
 
 elif user_question == "부산에서 가볼만한 관광지에 대해 추천해줘":
-    answer = 남포동3
+    answers = 남포동3
     location = [35.0971, 129.0346]  # (남포동)
 
 elif user_question == "부산에서 가볼만한 관광지에 대해 추천해줘":
-    answer = 태종대1
+    answers = 태종대1
     location = [35.0673, 129.0916]  # (태종대)
 
 elif user_question == "부산에서 가볼만한 관광지에 대해 추천해줘":
-    answer = 태종대2
+    answers = 태종대2
     location = [35.0673, 129.0916]  # (태종대)
 
 elif user_question == "부산에서 가볼만한 관광지에 대해 추천해줘":
-    answer = 송도해상케이블카1
+    answers = 송도해상케이블카1
     location = [35.0597, 129.1183]  # (송도)
 
 elif user_question == "부산에서 가볼만한 관광지에 대해 추천해줘":
-    answer = 송도해상케이블카2
+    answers = 송도해상케이블카2
     location = [35.0597, 129.1183]  # (송도)
 
 elif user_question == "부산에서 가볼만한 관광지에 대해 추천해줘":
-    answer = 감천문화마을1
+    answers = 감천문화마을1
     location = [35.0973, 129.0125]  # (감천)
 
 elif user_question == "부산에서 가볼만한 관광지에 대해 추천해줘":
-    answer = 감천문화마을2
+    answers = 감천문화마을2
     location = [35.0973, 129.0125]  # (감천)
 
 elif user_question == "부산에서 가볼만한 맛집에 대해 추천해줘":
-    answer = 해목1
+    answers = 해목1
     location = [35.0962, 129.04422]  # (해목)
 
 elif user_question == "부산에서 가볼만한 맛집에 대해 추천해줘":
-    answer = 선창횟집1
+    answers = 선창횟집1
     location = [35.1592, 129.1751]  # (선창횟집)
 
 elif user_question == "부산에서 가볼만한 맛집에 대해 추천해줘":
-    answer = 금수복국1
+    answers = 금수복국1
     location = [35.0952, 129.0342]  # (금수복국)
 
 elif user_question == "부산에서 가볼만한 맛집에 대해 추천해줘":
-    answer = 이재모피자1
+    answers = 이재모피자1
     location = [35.1592, 129.1751]  # (이재모피자)
 
 elif user_question == "부산에서 가볼만한 맛집에 대해 추천해줘":
-    answer = 할매국밥1
+    answers = 할매국밥1
     location = [35.0951, 129.0331]  # (할매국밥)
 
 elif user_question == "부산에서 가볼만한 자연/공원에 대해 추천해줘":
-    answer = 해운대해수욕장1
+    answers = 해운대해수욕장1
     location = [35.1586, 129.1600]  # (해운대 해수욕장)
 
 elif user_question == "부산에서 가볼만한 자연/공원에 대해 추천해줘":
-    answer = 해운대해수욕장2
+    answers = 해운대해수욕장2
     location = [35.1586, 129.1600]  # (해운대 해수욕장)
 
 elif user_question == "부산에서 가볼만한 자연/공원에 대해 추천해줘":
-    answer = 해운대해수욕장3
+    answers = 해운대해수욕장3
     location = [35.1586, 129.1600]  # (해운대 해수욕장)
 
 elif user_question == "부산에서 가볼만한 자연/공원에 대해 추천해줘":
-    answer = 광안리1
+    answers = 광안리1
     location = [35.1538, 129.1162]  # (광안리)
 
 elif user_question == "부산에서 가볼만한 자연/공원에 대해 추천해줘":
-    answer = 광안리2
+    answers = 광안리2
     location = [35.1538, 129.1162]  # (광안리)
 
 elif user_question == "부산에서 가볼만한 자연/공원에 대해 추천해줘":
-    answer = 기념공원1
+    answers = 기념공원1
     location = [35.1569, 129.1290]  # (기념공원)
 
 elif user_question == "부산에서 가볼만한 자연/공원에 대해 추천해줘":
-    answer = 기념공원2
+    answers = 기념공원2
     location = [35.1569, 129.1290]  # (기념공원)
 
 elif user_question == "부산에서 가볼만한 자연/공원에 대해 추천해줘":
-    answer = 기념공원3
+    answers = 기념공원3
     location = [35.1569, 129.1290]  # (기념공원)
 
 elif user_question == "부산에서 가볼만한 자연/공원에 대해 추천해줘":
-    answer = 용두산1
+    answers = 용두산1
     location = [35.1536, 129.1665]  # (용두산)
 
 elif user_question == "부산에서 가볼만한 자연/공원에 대해 추천해줘":
-    answer = 용두산2
+    answers = 용두산2
     location = [35.1536, 129.1665]  # (용두산)
 
 elif user_question == "부산에서 가볼만한 자연/공원에 대해 추천해줘":
-    answer = 송도1
+    answers = 송도1
     location = [35.2163, 128.6811]  # (송도)
 
 elif user_question == "부산에서 가볼만한 자연/공원에 대해 추천해줘":
-    answer = 송도2
+    answers = 송도2
     location = [35.2163, 128.6811]  # (송도)
 
 elif user_question == "부산에서 가볼만한 자연/공원에 대해 추천해줘":
-    answer = 송도3
+    answers = 송도3
     location = [35.2163, 128.6811]  # (송도)
 
 elif user_question == "부산에서 가볼만한 자연/공원에 대해 추천해줘":
-    answer = 이기대1
+    answers = 이기대1
     location = [35.1794, 129.0771]  # (이기대)
 
 elif user_question == "부산에서 가볼만한 자연/공원에 대해 추천해줘":
-    answer = 이기대2
+    answers = 이기대2
     location = [35.1794, 129.0771]  # (이기대)
 
 elif user_question == "부산에서 가볼만한 카페/디저트에 대해 추천해줘":
-    answer = 모모스커피1
+    answers = 모모스커피1
     location = [35.1548, 129.1634]  # (모모스커피 부산본점)
 
 elif user_question == "부산에서 가볼만한 카페/디저트에 대해 추천해줘":
-    answer = 모모스커피2
+    answers = 모모스커피2
     location = [35.1548, 129.16347]  # (모모스커피 부산본점)
 
 elif user_question == "부산에서 가볼만한 카페/디저트에 대해 추천해줘":
-    answer = 에테르1
+    answers = 에테르1
     location = [35.1579, 129.1610]  # (에테르)
 
 elif user_question == "부산에서 가볼만한 카페/디저트에 대해 추천해줘":
-    answer = 에테르2
+    answers = 에테르2
     location = [35.1579, 129.1610]  # (에테르)
 
 elif user_question == "부산에서 가볼만한 카페/디저트에 대해 추천해줘":
-    answer = 초량1
+    answers = 초량1
     location = [35.1071, 129.0336]  # (초량1941)
 
 elif user_question == "부산에서 가볼만한 카페/디저트에 대해 추천해줘":
-    answer = 초량2
+    answers = 초량2
     location = [35.1071, 129.0336]  # (초량1941)
 
 elif user_question == "부산에서 가볼만한 카페/디저트에 대해 추천해줘":
-    answer = 코랄라니1
+    answers = 코랄라니1
     location = [35.1466, 129.1071]  # (코랄라니)
 
 elif user_question == "부산에서 가볼만한 카페/디저트에 대해 추천해줘":
-    answer = 코랄라니2
+    answers = 코랄라니2
     location = [35.1466, 129.1071]  # (코랄라니)
 
 elif user_question == "부산에서 가볼만한 카페/디저트에 대해 추천해줘":
-    answer = 오션브리즈1
+    answers = 오션브리즈1
     location = [35.1584, 129.1629]  # (오션브리즈)
 
 elif user_question == "부산에서 가볼만한 카페/디저트에 대해 추천해줘":
-    answer = 볼트1
+    answers = 볼트1
     location = [35.1495, 129.1232]  # (볼트)
 
 elif user_question == "부산에서 가볼만한 카페/디저트에 대해 추천해줘":
-    answer = 볼트2
+    answers = 볼트2
     location = [35.1495, 129.1232]  # (볼트)
 
 # 지도를 생성합니다.
@@ -497,6 +489,7 @@ m = folium.Map(location=[35.17944, 129.07556], zoom_start=12)  # 초기 위치�
 # 위치 정보가 있는 경우 마커를 추가합니다.
 if location:
     folium.Marker(location, tooltip=answer).add_to(m)
+    marker.add_to(m)
 
 # Streamlit에 Folium 맵을 표시합니다.
 st_folium(m)
