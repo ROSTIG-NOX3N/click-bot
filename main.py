6b92cc40cec7ebfdf844e92d2d72d972bc94de2e
ROSTIG-NOX3N <137249382+ROSTIG-NOX3N@users.noreply.github.com>
2023-09-24 16:35:08 +0900

    Update main.py

--- main.py
+++ main.py
@@ -23,10 +23,13 @@ from streamlit_folium import st_folium
 from streamlit_option_menu import option_menu
 from streamlit_js_eval import get_browser_language
 
+import streamlit as st
+import random
+
 class ChatBot:
     def __init__(self):
         self.responses = {
-            "---질문 선택---": "",
+            "---질문 선택---: , "질문을 선택하고 답변해주세요"
             "안녕": "안녕하세요!",
             "날씨 어때?": "오늘은 맑아요.",
             "이름이 뭐야?": "저는 챗봇이에요."
@@ -39,19 +42,14 @@ class ChatBot:
 chatbot = ChatBot()
 
 # Streamlit 애플리케이션 정의
-st.title("선택형 챗봇")
-
-# 선택한 질문을 저장할 변수
-selected_question = st.empty()
+st.title("버튼 선택형 챗봇")
 
 # 질문 선택을 위한 버튼
-if selected_question.button("질문 선택"):
-    selected_question.selectbox("질문 선택:", list(chatbot.responses.keys()))
+selected_question = st.selectbox("질문 선택:", list(chatbot.responses.keys()))
 
 # 버튼 클릭 이벤트 처리
 if st.button("챗봇에게 질문"):
-    question = selected_question.selectbox("질문 선택:", list(chatbot.responses.keys()))
-    bot_response = chatbot.get_response(question)
+    bot_response = chatbot.get_response(selected_question)
 
     # 챗봇 응답 출력
     st.write("챗봇 응답:", bot_response)
