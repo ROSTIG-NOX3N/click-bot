5803e8f4b2466939efe0b0bc6e79fa7d070e8f9c
ROSTIG-NOX3N <137249382+ROSTIG-NOX3N@users.noreply.github.com>
2023-09-24 16:31:55 +0900

    Update main.py

--- main.py
+++ main.py
@@ -25,44 +25,27 @@ from streamlit_js_eval import get_browser_language
 
 class ChatBot:
     def __init__(self):
-        self.responses = {}
-        self.last_question = None
-
-    def add_response(self, question, answer):
-        self.responses[question] = answer
+        self.responses = {
+            "안녕": "안녕하세요!",
+            "날씨 어때?": "오늘은 맑아요.",
+            "이름이 뭐야?": "저는 챗봇이에요."
+        }
 
     def get_response(self, question):
-        if question == self.last_question:
-            response = random.choice(list(self.responses.values()))
-        else:
-            response = self.responses.get(question, "그 질문에 대한 답변을 모르겠어요.")
-        self.last_question = question
-        return response
+        return self.responses.get(question, "그 질문에 대한 답변을 모르겠어요.")
 
 # 챗봇 인스턴스 생성
 chatbot = ChatBot()
 
-# 예제 답변 추가
-chatbot.add_response("안녕", "안녕하세요!")
-chatbot.add_response("날씨 어때?", "오늘은 맑아요.")
-chatbot.add_response("이름이 뭐야?", "저는 챗봇이에요.")
-
 # Streamlit 애플리케이션 정의
-def main():
-    st.title("버튼 클릭형 챗봇")
+st.title("버튼 선택형 챗봇")
 
-    # 사용자 입력 받기
-    user_input = st.text_input("사용자 입력:")
+# 질문 선택을 위한 버튼
+selected_question = st.selectbox("질문 선택:", list(chatbot.responses.keys()))
 
-    # 버튼 클릭 이벤트 처리
-    if st.button("챗봇에게 질문"):
-        if user_input:
-            # 사용자 입력을 챗봇에 전달하고 응답 받기
-            bot_response = chatbot.get_response(user_input)
+# 버튼 클릭 이벤트 처리
+if st.button("챗봇에게 질문"):
+    bot_response = chatbot.get_response(selected_question)
 
-            # 챗봇 응답 출력
-            st.write("챗봇 응답:", bot_response)
-
-if __name__ == "__main__":
-    main()
-
+    # 챗봇 응답 출력
+    st.write("챗봇 응답:", bot_response)
