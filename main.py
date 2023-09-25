b8849438cb17dca3b1d5dac92b518538cf509ac5
ROSTIG-NOX3N <137249382+ROSTIG-NOX3N@users.noreply.github.com>
2023-09-25 23:23:46 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,17 @@
 import streamlit as st
 import pandas as pd
-import random
-import openpyxl
 
-excel_file_path = "data.xlsx"  # Excel 파일의 경로를 지정해야 합니다.
-df = pd.read_excel(excel_file_path)
+# Excel 파일의 경로를 지정해야 합니다.
+excel_file_path = "data.xlsx"
 
 # Streamlit 앱 시작
 st.title('데이터 프레임 표시')
 
-# 데이터 프레임을 표로 표시
-st.write(df)
+# Excel 파일을 데이터 프레임으로 읽어오기
+try:
+    df = pd.read_excel(excel_file_path)
+    st.write(df)
+except FileNotFoundError:
+    st.error(f"파일을 찾을 수 없습니다: {excel_file_path}")
+except Exception as e:
+    st.error(f"데이터 프레임을 로드하는 중 오류 발생: {e}")
